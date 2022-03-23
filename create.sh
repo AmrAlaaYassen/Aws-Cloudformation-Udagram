e68d50a8d760551329a628f350d1abe4dfff94de
amr alaa yassen <amralaa566@gmail.com>
2022-03-23 23:28:27 +0200

    add application servers & load balancer

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
 # a script to easily create aws cloud formation stack 
-aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1 --on-failure DO_NOTHING
