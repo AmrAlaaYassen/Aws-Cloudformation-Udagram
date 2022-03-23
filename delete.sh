b6968a2b04fad25523ad722582035532ea2243bd
amr alaa yassen <amralaa566@gmail.com>
2022-03-23 16:36:51 +0200

    add VPC resources and params

---
+++ delete.sh
@@ -0,0 +1,2 @@
+# a script to easily delete aws cloud formation stack 
+aws cloudformation delete-stack --stack-name $1
---
+++ update.sh
@@ -0,0 +1,2 @@
+# a script to easily update aws cloud formation stack
+aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-1
